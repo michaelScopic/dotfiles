ec38d5de49dd3081d4a2f2683d488fe4e2132091
michaelScopic <michael@sanbornhome.com>
2022-10-26 10:39:29 -0700

    added `init.sh`

---
+++ zsh/init.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# * Initalizer script
+
+echo -e " 
++-----------------------+
+| Initalizing script... |
++-----------------------+ "
+sleep 0.5
+
+currentDir=$(pwd)
+echo -e "Current directory: $currentDir"
+echo -e ""
+
+# -- Set colors --
+echo -e " --- Setting colors... ---"
+reset='\e[0m' && echo -e "$reset Normal text"
+red='\e[31m' && echo -e "$red Red $reset"
+green='\e[32m' && echo -e "$green Green $reset"
+yellow='\e[33m' && echo -e "$yellow Yellow $reset"
+blue='\e[34m' && echo -e "$blue Blue $reset"
+purple='\e[35m' && echo -e "$purple Purple $reset"
+cyan='\e[36m' && echo -e "$cyan Cyan $reset"
+sleep 0.3
+echo -e ""
+redbg='\e[41m' && echo -e "$redbg Red background $reset"
+greenbg='\e[42m' && echo -e "$greenbg Green background $reset"
+yellowbg='\e[43m' && echo -e "$yellowbg Yellow background $reset"
+bluebg='\e[44m' && echo -e "$bluebg Blue background $reset"
+purplebg='\e[45m' && echo -e "$purplebg Purple background $reset"
+cyanbg='\e[46m' && echo -e "$cyanbg Cyan background $reset"
+echo -e "
++-----------------------+
+| $green Done setting colors! $reset|
++-----------------------+"
+
+sleep 0.5
+echo -e ""
+
+# --- Print out basic info ---
+echo -e "---------- Basic info ----------"
+# Print the distro 
+echo -e   "$green Distro:$reset $(lsb_release -d | cut -f 2- )"
+# Print kernel version
+echo -e "$yellow Kernel:$reset $(uname -r) $(uname -m)" 
+# Print shell 
+echo -e "$blue Shell:$reset $SHELL"
+# Print hostname
+echo -e "$purple Hostname:$reset $(cat /etc/hostname)"
+# Print current user (is user root?)
+echo -e "$cyan User:$reset $(whoami)"
+echo -e "--------------------------------"
+
+echo -e "
+######################################
+#$green        Done initalizing! $reset          #
+#$red Continuing with original script...$reset #
+######################################"
+
+sleep 2
--- zsh/pluginInstall.sh
+++ zsh/pluginInstall.sh
@@ -1,39 +1,41 @@
-#!/usr/bin/env bash 
+#!/bin/bash 
 
+
+. init.sh
 # print zsh version
-echo -e "\e[46mZSH version:\e[0m $(zsh --version)" ; sleep 1
+echo -e "$yellow ZSH version:$reset $(zsh --version)" ; sleep 1
 
 # create the plugins directory
 echo -e "
 #################################
-# \e[33mCreating a directory for \e[0m     #
-# \e[33mplugins in: \e[32m~/.zsh-plugins...\e[0m #
+#$blue Creating a directory for $reset     #
+#$blue plugins in:$red ~/.zsh-plugins...$reset #
 #################################" 
 mkdir ~/.zsh-plugins 2>/dev/null
 sleep 1
-echo -e "\e[33mDone. Going to install plugins...\e[0m"
+echo -e "$greenbg Done. Going to install plugins...$reset"
 
 # clone the plugins themselves
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh-plugins/zsh-autosuggestions 2>/dev/null
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.zsh-plugins/fsh 2>/dev/null
-echo -e "\e[32mFinished installing plugins...\e[0m"
+echo -e "$greenbg Finished installing plugins...$reset"
 # make a backup of user's .zshrc and place my .zshrc in their ~/.zshrc
 echo -e "
 ########################################
-# \e[31mDo you want to overwrite your \e[0m       #
-# \e[31m.zshrc with the one from here?  \e[0m     #
-# \e[31mI will make a backup of your current \e[0m#
-# \e[31mone called \e[33m'~/.zshrc.bak' \e[0m           #
+#$red Do you want to overwrite your $reset       #
+#$red .zshrc with the one from here?  $reset     #
+#$cyan I will make a backup of your current $reset#
+#$cyan one called$purple '~/.zshrc.bak' $reset           #
 ########################################"
 read -p "Overwrite? [Y/n]: " option
 if [ "$option" != "n" ]
 then
     mv -v $HOME/.zshrc $HOME/.zshrc.bak 2>/dev/null
     cp -v zshrc $HOME/.zshrc
-    echo -e "\e[32m Done! Reloading your shell...\e[0m"
+    echo -e "$green Done! Reloading your shell...$reset"
     sleep 1 ; exec zsh
 else
-    echo -e "\e[31mOk, not overwriting. Exiting...\e[0m"
+    echo -e "$redbg Ok, not overwriting. Exiting...$reset"
 fi
 
 exit 0
