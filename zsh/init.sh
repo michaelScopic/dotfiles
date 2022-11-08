e7b7a33c75c44f5f6c40b74ebada3ff9910d588d
Michael_Scopic <michael@sanbornhome.com>
2022-11-08 19:05:53 +0000

    cleaned up some code, added starship install

--- zsh/init.sh
+++ zsh/init.sh
@@ -8,25 +8,45 @@ echo -e "
 +-----------------------+ "
 sleep 0.5
 
-currentDir=$(pwd)
-echo -e "Current directory: $currentDir"
-echo -e ""
 
-# -- Set colors --
+# --- Set colors ---
 echo -e " --- Setting colors... ---"
-reset='\e[0m' && echo -e "$reset Normal text"
-red='\e[31m' && echo -e "$red Red $reset"; redbg='\e[41m' && echo -e "$redbg Red background $reset"
-green='\e[32m' && echo -e "$green Green $reset"; greenbg='\e[42m' && echo -e "$greenbg Green background $reset"
-yellow='\e[33m' && echo -e "$yellow Yellow $reset"; yellowbg='\e[43m' && echo -e "$yellowbg Yellow background $reset"
-blue='\e[34m' && echo -e "$blue Blue $reset"; bluebg='\e[44m' && echo -e "$bluebg Blue background $reset"
-purple='\e[35m' && echo -e "$purple Purple $reset"; purplebg='\e[45m' && echo -e "$purplebg Purple background $reset"
-cyan='\e[36m' && echo -e "$cyan Cyan $reset"; cyanbg='\e[46m' && echo -e "$cyanbg Cyan background $reset"
+# Normal text
+reset='\e[0m' && echo -e "${reset}Normal text"
+
+# Bold text
+bold='\e[1m' && echo -e "${bold}Bold text ${reset}"
+
+# Red 
+red='\e[31m' && echo -e "${red}Red ${reset}"; \
+    redbg='\e[41m' && echo -e "${redbg}Red background $reset"
+
+# Green
+green='\e[32m' && echo -e "${green}Green ${reset}"; \
+    greenbg='\e[42m' && echo -e "${greenbg}Green background ${reset}"
+    
+# Yellow
+yellow='\e[33m' && echo -e "${yellow}Yellow ${reset}"; \
+    yellowbg='\e[43m' && echo -e "${yellowbg}Yellow background ${reset}"
+
+# Blue
+blue='\e[34m' && echo -e "${blue}Blue ${reset}"; \
+    bluebg='\e[44m' && echo -e "${bluebg}Blue background ${reset}"
+
+# Purple
+purple='\e[35m' && echo -e "${purple}Purple ${reset}"; \
+    purplebg='\e[45m' && echo -e "${purplebg}Purple background ${reset}"
+    
+# Cyan
+cyan='\e[36m' && echo -e "${cyan}Cyan ${reset}"; \
+    cyanbg='\e[46m' && echo -e "${cyanbg}Cyan background ${reset}"
+
 sleep 0.3
 
 echo -e "
-+-----------------------+
-| $green Done setting colors! $reset|
-+-----------------------+"
++----------------------+
+| ${green}Done setting colors! ${reset}|
++----------------------+"
 
 sleep 0.5
 echo -e ""
@@ -34,15 +54,20 @@ echo -e ""
 # --- Print out basic info ---
 echo -e "---------- Basic info ----------"
 # Print the distro 
-echo -e   "$green Distro:$reset $(lsb_release -d | cut -f 2- )"
+echo -e "${green}${bold}Distro:${reset} $(lsb_release -d | cut -f 2- )"
 # Print kernel version
-echo -e "$yellow Kernel:$reset $(uname -r) $(uname -m)" 
+echo -e "${yellow}${bold}Kernel:${reset} $(uname -srm)" 
 # Print shell 
-echo -e "$blue Shell:$reset $SHELL"
+echo -e "${blue}${bold}Shell:${reset} $SHELL"
 # Print hostname
-echo -e "$purple Hostname:$reset $(cat /etc/hostname)"
+echo -e "${purple}${bold}Hostname:${reset} $(cat /etc/hostname)"
 # Print current user (is user root?)
-echo -e "$cyan User:$reset $(whoami)"
+echo -e "${cyan}${bold}User:${reset} $(whoami)"
 echo -e "--------------------------------"
 echo ""
-echo -e "$greenbg Done. $reset"
+
+# --- Store this directory as a variable ---
+currentDir=$(pwd)
+echo -e "${yellow}Current directory is:${reset} $currentDir \n"
+
+echo -e "${greenbg}Done initalizing.${reset}"
--- zsh/pluginInstall.sh
+++ zsh/pluginInstall.sh
@@ -3,39 +3,65 @@
 
 . init.sh
 # print zsh version
-echo -e "$yellow ZSH version:$reset $(zsh --version)" ; sleep 1
+echo -e "${yellow}ZSH version:${reset} $(zsh --version)" ; sleep 1
 
 # create the plugins directory
 echo -e "
 #################################
-#$blue Creating a directory for $reset     #
-#$blue plugins in:$red ~/.zsh-plugins...$reset #
+# ${blue}Creating a directory for ${reset}     #
+# ${blue}plugins in:${red} ~/.zsh-plugins...${reset} #
 #################################" 
+
+echo -e "
+###################################
+#${yellow} Plugins that will be installed:${reset} #
+#${blue}  fast-syntax-highlighting ${reset}      #
+#${blue}  zsh-autosuggestions ${reset}           #
+###################################" && sleep 2
+
 mkdir ~/.zsh-plugins 2>/dev/null
 sleep 1
-echo -e "$greenbg Done. Going to install plugins...$reset"
+echo -e "${greenbg}Done. Going to install plugins...${reset}"
 
 # clone the plugins themselves
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh-plugins/zsh-autosuggestions 2>/dev/null
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.zsh-plugins/fsh 2>/dev/null
-echo -e "$greenbg Finished installing plugins...$reset"
+echo -e "${greenbg}Finished installing plugins...${reset}"
+
+sleep 1
 # make a backup of user's .zshrc and place my .zshrc in their ~/.zshrc
 echo -e "
 ########################################
-#$red Do you want to overwrite your $reset       #
-#$red .zshrc with the one from here?  $reset     #
-#$cyan I will make a backup of your current $reset#
-#$cyan one called$purple '~/.zshrc.bak' $reset           #
+# ${red}${bold}Do you want to overwrite your ${reset}       #
+# ${red}${bold}.zshrc with my zshrc?  ${reset}              #
+# ${cyan}I will make a backup of your current ${reset}#
+# ${cyan}one called${purple} '~/.zshrc.bak' ${reset}           #
 ########################################"
-read -p "Overwrite? [Y/n]: " option
-if [ "$option" != "n" ]
+read -p "Overwrite? [Y/n]: " zsh_overwrite
+
+if [ "$zsh_overwrite" != "n" ]
 then
     mv -v $HOME/.zshrc $HOME/.zshrc.bak 2>/dev/null
     cp -v zshrc $HOME/.zshrc
-    echo -e "$green Done! Reloading your shell...$reset"
-    sleep 1 ; exec zsh
+    echo -e "${greenbg}Done!${reset}"
 else
-    echo -e "$redbg Ok, not overwriting. Exiting...$reset"
+    echo -e "${redbg}Ok, ${bold}not${reset}${redbg} overwriting.${reset}"
 fi
+
+sleep 1
+# --- Install starship prompt ---
+echo -e "${cyan}${bold}Do you want to install the starship prompt?${reset}"
+
+read -p "Install starship? [Y/n]" install_starship
+if [ "$install_starship" != "n" ]
+then
+    echo -e "${green}Ok. Installing Starship prompt...${reset}"
+    curl -sS https://starship.rs/install.sh | sh
+else 
+    echo -e "${red}Ok. ${bold}not${reset}${red} installing Starship."
+fi
+
+echo -e "${greenbg}Done with everything! Reloading your shell for you! Enjoy!${reset}"
+exec zsh
 
 exit 0
