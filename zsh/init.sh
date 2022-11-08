09495834d68eec0c143ce9b83738a8c3580ff1a4
michaelScopic <michael@sanbornhome.com>
2022-11-08 08:57:36 -0800

    optimized `init.sh`

--- zsh/init.sh
+++ zsh/init.sh
@@ -15,20 +15,14 @@ echo -e ""
 # -- Set colors --
 echo -e " --- Setting colors... ---"
 reset='\e[0m' && echo -e "$reset Normal text"
-red='\e[31m' && echo -e "$red Red $reset"
-green='\e[32m' && echo -e "$green Green $reset"
-yellow='\e[33m' && echo -e "$yellow Yellow $reset"
-blue='\e[34m' && echo -e "$blue Blue $reset"
-purple='\e[35m' && echo -e "$purple Purple $reset"
-cyan='\e[36m' && echo -e "$cyan Cyan $reset"
+red='\e[31m' && echo -e "$red Red $reset"; redbg='\e[41m' && echo -e "$redbg Red background $reset"
+green='\e[32m' && echo -e "$green Green $reset"; greenbg='\e[42m' && echo -e "$greenbg Green background $reset"
+yellow='\e[33m' && echo -e "$yellow Yellow $reset"; yellowbg='\e[43m' && echo -e "$yellowbg Yellow background $reset"
+blue='\e[34m' && echo -e "$blue Blue $reset"; bluebg='\e[44m' && echo -e "$bluebg Blue background $reset"
+purple='\e[35m' && echo -e "$purple Purple $reset"; purplebg='\e[45m' && echo -e "$purplebg Purple background $reset"
+cyan='\e[36m' && echo -e "$cyan Cyan $reset"; cyanbg='\e[46m' && echo -e "$cyanbg Cyan background $reset"
 sleep 0.3
-echo -e ""
-redbg='\e[41m' && echo -e "$redbg Red background $reset"
-greenbg='\e[42m' && echo -e "$greenbg Green background $reset"
-yellowbg='\e[43m' && echo -e "$yellowbg Yellow background $reset"
-bluebg='\e[44m' && echo -e "$bluebg Blue background $reset"
-purplebg='\e[45m' && echo -e "$purplebg Purple background $reset"
-cyanbg='\e[46m' && echo -e "$cyanbg Cyan background $reset"
+
 echo -e "
 +-----------------------+
 | $green Done setting colors! $reset|
@@ -50,11 +44,5 @@ echo -e "$purple Hostname:$reset $(cat /etc/hostname)"
 # Print current user (is user root?)
 echo -e "$cyan User:$reset $(whoami)"
 echo -e "--------------------------------"
-
-echo -e "
-######################################
-#$green        Done initalizing! $reset          #
-#$red Continuing with original script...$reset #
-######################################"
-
-sleep 2
+echo ""
+echo -e "$greenbg Done. $reset"
