097a82342305716d0cbfadea9f69e1dffdc82ef7
Michael_Scopic <michael@sanbornhome.com>
2022-12-06 21:52:50 +0000

    added an info agrument

--- deploy.sh
+++ deploy.sh
@@ -209,6 +209,7 @@ function usage() {
     echo -e "${blue}${bold}plugins${reset}   ->     ${cyan}Just install ZSH plugins and dependencies${reset}"
     echo -e "${blue}${bold}backup${reset}    ->     ${cyan}Just backup user's current configs (htop, kitty, neofetch, starship prompt)${reset}"
     echo -e "${blue}${bold}overwrite${reset} ->     ${cyan}Just overwrite user's current configs with the ones in this repo${reset}"
+    echo -e "${blue}${bold}info${reset}      ->     ${cyan}Print some basic info of this machine${reset}"
     echo -e "${blue}${bold}help${reset}      ->     ${cyan}Print this menu${reset}"
 
     return 1
@@ -236,18 +237,24 @@ case $1 in
 
     backup) ## Just call the backup funct
         init_script
-        source "$dotfilesLoc"/scripts/info.sh
+        bash -c "$dotfilesLoc"/scripts/info.sh
         backup
         exit
     ;;
 
     overwrite) ## Call the overwrite funct
         init_script
-        source "$dotfilesLoc"/scripts/info.sh
+        bash -c "$dotfilesLoc"/scripts/info.sh
         overwrite
         exit
     ;;
 
+    info) 
+        init_script &>/dev/null
+        bash -c "$dotfilesLoc"/scripts/info.sh
+        exit
+    ;;
+    
     help)   ## Print the help section 
         init_script &>/dev/null
         usage
