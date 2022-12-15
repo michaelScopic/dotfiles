c17e21d9e4f22d1b3c6695ffa297f9686966bd07
michaelScopic <michael@sanbornhome.com>
2022-12-15 09:39:03 -0800

    uploaded NvChad config

---
+++ config/nvim/custom/chadrc.lua
@@ -0,0 +1,27 @@
+-- Chadrc overrides this file
+
+local M = {}
+
+M.options = {
+  nvChad = {
+    update_url = "https://github.com/NvChad/NvChad",
+    update_branch = "main",
+  },
+}
+
+M.ui = {
+  -- hl = highlights
+  hl_add = {},
+  hl_override = {},
+  changed_themes = {},
+  theme_toggle = { "onenord", "palenight", "decay", "mountain" },
+  theme = "nord", -- default theme
+  transparency = false,
+}
+
+M.plugins = {}
+
+-- check core.mappings for table structure
+M.mappings = require "core.mappings"
+
+return M
