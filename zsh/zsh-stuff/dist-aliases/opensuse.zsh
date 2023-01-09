189ff804e992af41e4142ba08bd3307b0cdf356c
michaelScopic <michael@sanbornhome.com>
2023-01-09 14:46:48 -0800

    new alias name for opensuse

--- zsh/zsh-stuff/dist-aliases/opensuse.zsh
+++ zsh/zsh-stuff/dist-aliases/opensuse.zsh
@@ -1,7 +1,7 @@
 # === openSUSE aliases for package management ===
 ## You are always welcome to add/edit your own aliases :)
 alias up="sudo zypper up"
-alias dist-update="sudo zypper dup"
+alias dup="sudo zypper dup"
 alias zypp-ref="sudo zypper ref -f"
 alias zypp-clean="sudo zypper cc; sudo zypper cl"
 alias update-mirrors="sudo mirrorsorcerer -x"
