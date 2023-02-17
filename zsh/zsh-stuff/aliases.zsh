ac3d30d251b6846b662609dcb226738c452d397f
michaelScopic <michael@sanbornhome.com>
2023-02-17 12:35:06 -0800

    fixed some stuff

--- zsh/zsh-stuff/aliases.zsh
+++ zsh/zsh-stuff/aliases.zsh
@@ -2,11 +2,16 @@
 
 ### Written by: michaelScopic (https://github.com/michaelScopic)
 
-DISTRO_ALIAS_DIR="$HOME/.config/zsh/dist-aliases" ## <- Directory for distro-specific aliases
+#### Set directory for distro specific aliases
+DISTRO_ALIAS_DIR="$HOME/.config/zsh/dist-aliases" 
 
-#### Uncomment below to expand aliases as you type them
+#### Expand aliases as you type them
 #bindkey -- ' ' exp_alias
-## eg: typing 'ls' then pressing space will make it appear as 'exa $EXA_OPTIONS'
+
+#### Default to vim if $EDITOR is not set
+if [ -z $EDITOR ]; then
+    export $EDITOR=vim
+fi
 
 #### --- Set aliases according to terminal ---
 if [ -t 0 ]; then
@@ -34,23 +39,23 @@ alias grep="grep --color=auto"
 #### - Booting/GRUB -
 alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
 alias grub-config="sudo grub-configurator"
-alias edit-fstab="sudo vim /etc/fstab"
+alias edit-fstab="sudo $EDITOR /etc/fstab"
 alias cum="echo -e 'System rebooting...\nGoodbye!'; sudo reboot"
 alias kys="echo -e 'System shutting down...\nGoodbye!'; sudo shutdown now"
 
 #### - Config editing -
-alias zshrc="vim ~/.zshrc"
-alias zsh-aliases="vim ~/.config/zsh/aliases.zsh"
+alias zshrc="$EDITOR ~/.zshrc"
+alias zsh-aliases="$EDITOR ~/.config/zsh/aliases.zsh"
 alias ls-aliases="cat ~/.config/zsh/aliases.zsh"
-alias zsh-functs="vim ~/.config/zsh/user-functions.zsh"
-alias kitty-config="vim ~/.config/kitty/kitty.conf"
-alias chadwmrc="vim ~/.config/chadwm/scripts/run.sh"
-alias chadwm-config="vim ~/.config/chadwm/chadwm/config.def.h"
+alias zsh-functs="$EDITOR ~/.config/zsh/user-functions.zsh"
+alias kitty-config="$EDITOR ~/.config/kitty/kitty.conf"
+alias chadwmrc="$EDITOR ~/.config/chadwm/scripts/run.sh"
+alias chadwm-config="$EDITOR ~/.config/chadwm/chadwm/config.def.h"
 
 #### - Networking -
-alias virt-man_net_start="sudo virsh net-start default" ## <-- For virt-manager
+alias virt-man_net_start="sudo virsh net-start default" ## <- For virt-manager
 alias nmcli="nmcli -p"                                  ## <- Make nmcli pretty
-alias ip="ip -c"
+alias ip="ip -c"                                        ## <- Give 'ip' color
 alias nm-stat="nmcli dev status"
 alias ls-wifi="nmcli dev wifi list"
 alias wifi-restart="nmcli radio wifi off && nmcli radio wifi on"
